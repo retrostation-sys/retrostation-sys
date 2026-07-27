0df2f87cdca00c4c52251b0fe3c557e478be9747
retrostation-sys <retrostationperoiz74@gmail.com>
2026-07-27 11:26:13 -0600

    Add files via upload

---
+++ juegos-atari2600.js
@@ -0,0 +1,30 @@
+agregarJuegos([
+    { 
+        title: "CAVE 1K", 
+        img: "https://drive.google.com/thumbnail?id=10Z-aAZ1-0z_XpTa20joELFHxvHlz7I8G", 
+        src: "cave1k.bin", 
+        system: "atari2600" 
+    },
+    { 
+        title: "CLIMBER 5", 
+        img: "https://drive.google.com/thumbnail?id=1JvXB20Dmx37kUCSa6l22U-yumGFG-s4X", 
+        src: "climber5.bin", 
+        system: "atari2600" 
+    },
+    { 
+        title: "PAC MAN", 
+        img: "https://drive.google.com/thumbnail?id=1K42axvcoK9pbt6bG31wmHL6_91TCNcq_", 
+        src: "pacman4k(atari2600).bin", 
+        system: "atari2600" 
+    },
+    { 
+        title: "ADVENTURE", 
+        img: "https://drive.google.com/thumbnail?id=1-RHIk1YmYAOinCRSiBfr_BZQeZOXjWUv", 
+        system: "atari2600" 
+    },
+    { 
+        title: "ALIEN", 
+        img: "https://drive.google.com/thumbnail?id=1ZQUBZCTArYc0e565TorX8pl_pas-oBh5", 
+        system: "atari2600" 
+    }
+]);
