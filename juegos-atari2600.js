e6f9be2c6f222482d3b0cc36f3a401240212e9d0
retrostation-sys <retrostationperoiz74@gmail.com>
2026-07-28 08:31:33 -0600

    Add files via upload

--- juegos-atari2600.js
+++ juegos-atari2600.js
@@ -1,30 +1,56 @@
+x= "https://drive.google.com/thumbnail?id="
 agregarJuegos([
-    { 
-        title: "CAVE 1K", 
-        img: "https://drive.google.com/thumbnail?id=10Z-aAZ1-0z_XpTa20joELFHxvHlz7I8G", 
-        src: "cave1k.bin", 
-        system: "atari2600" 
-    },
-    { 
-        title: "CLIMBER 5", 
-        img: "https://drive.google.com/thumbnail?id=1JvXB20Dmx37kUCSa6l22U-yumGFG-s4X", 
-        src: "climber5.bin", 
-        system: "atari2600" 
-    },
-    { 
-        title: "PAC MAN", 
-        img: "https://drive.google.com/thumbnail?id=1K42axvcoK9pbt6bG31wmHL6_91TCNcq_", 
-        src: "pacman4k(atari2600).bin", 
-        system: "atari2600" 
-    },
-    { 
-        title: "ADVENTURE", 
-        img: "https://drive.google.com/thumbnail?id=1-RHIk1YmYAOinCRSiBfr_BZQeZOXjWUv", 
-        system: "atari2600" 
-    },
-    { 
-        title: "ALIEN", 
-        img: "https://drive.google.com/thumbnail?id=1ZQUBZCTArYc0e565TorX8pl_pas-oBh5", 
-        system: "atari2600" 
-    }
+    { title: "CAVE 1K", img: x+ "10Z-aAZ1-0z_XpTa20joELFHxvHlz7I8G", src: "cave1k.bin", system: "atari2600" },
+    { title: "CLIMBER 5", img: "https://drive.google.com/thumbnail?id=1JvXB20Dmx37kUCSa6l22U-yumGFG-s4X", src: "climber5.bin", system: "atari2600" },
+    { title: "PAC MAN", img: "https://drive.google.com/thumbnail?id=1K42axvcoK9pbt6bG31wmHL6_91TCNcq_", src: "pacman4k(atari2600).bin", system: "atari2600" },
+    { title: "SOCCER", img: "https://drive.google.com/thumbnail?id=16C0nrVvgcY6gU7Lnp-C-88kFZzeOHRsd", src: "soccer(atari2600).bin", system: "atari2600" },
+    { title: "SPLATFORM", img: "https://drive.google.com/thumbnail?id=1GW0O_Di335YfK6AkJjfe5OHh_J1aOyAI", src: "splatform2600.bin", system: "atari2600" },
+    
+    
+    { title: "ADVENTURE", img: "https://drive.google.com/thumbnail?id=1-RHIk1YmYAOinCRSiBfr_BZQeZOXjWUv", system: "atari2600" },
+    { title: "ALIEN", img: "https://drive.google.com/thumbnail?id=1ZQUBZCTArYc0e565TorX8pl_pas-oBh5", system: "atari2600" },
+    { title: "AMIDAR", img: "https://drive.google.com/thumbnail?id=1HQ7JMkVQ7b4avH3EuYDgyN8IecY_Sqke", system: "atari2600" },
+    { title: "ASSAULT", img: "https://drive.google.com/thumbnail?id=19Ut-0tXeVil8OK1YE7-8NTa7qv4XrRaO", system: "atari2600" },
+    { title: "ASTEROIDS", img: "https://drive.google.com/thumbnail?id=1SpOj85GKmJVoSSXrsCC0xm-4zITznAA5", system: "atari2600" },
+    { title: "ATARI VIDEO CUBE", img: "https://drive.google.com/thumbnail?id=1QjcNMKPiiPYVpu05TdAzBLXt2KrmU-8Y", system: "atari2600" },
+    { title: "BANK HEIST", img: "https://drive.google.com/thumbnail?id=1Ev2L_wVo17q428Kj3gToaAnFZt-pP2_z", system: "atari2600" },
+    { title: "BATTLEZONE", img: "https://drive.google.com/thumbnail?id=1RpP3z-pHVU165rrPcIyWchbWzmjZ0FWo", system: "atari2600" },
+    { title: "BEANY BOPPER", img: "https://drive.google.com/thumbnail?id=1XkMXVcdJKqpK4UEhcauNauABFdZ7KPbU", system: "atari2600" },
+    { title: "BERMUDA", img: "https://drive.google.com/thumbnail?id=1gxB1A5Ug5XcBuKhoh3SLhZzrFqtqS5yy", system: "atari2600" },
+    { title: "BERZERK", img: "https://drive.google.com/thumbnail?id=12kycCuNWsXUL2Y-F9todX5rwucWxNHSq", system: "atari2600" },
+    { title: "CAT TRAX", img: "https://drive.google.com/thumbnail?id=1J5CxK3JdOChuKnw3JQShUxcWcCkBB7cO", system: "atari2600" },
+    { title: "CENTIPEDE", img: "https://drive.google.com/thumbnail?id=1_FAflb8Nhkg8gv96Ea4uimtUzGse8Qov", system: "atari2600" },
+    { title: "CHOPPER COMMAND", img: "https://drive.google.com/thumbnail?id=1p1j1aUYbIZZnLVrs1c-WPErTS-JeUwdQ", system: "atari2600" },
+    { title: "CONQUEST OF MARS", img: "https://drive.google.com/thumbnail?id=1i3Jav46wlMM9vORyuJvE1gsvGVTsIWse", system: "atari2600" },
+    { title: "CRYSTAL CASTLES", img: "https://drive.google.com/thumbnail?id=12ZpyarN54IQHijFO09jP3xbE0ukOk6a0", system: "atari2600" },
+    { title: "DARK CAVERN", img: "https://drive.google.com/thumbnail?id=144U3WAEoQ0gOUdZqZuOyH1RF1J4_Oa45", system: "atari2600" },
+    { title: "DARK CHAMBERS", img: "https://drive.google.com/thumbnail?id=1lbtiQnoGfu4UsWO_kK7LeaWjs-r_SE2s", system: "atari2600" },
+    { title: "DEFENDER", img: "https://drive.google.com/thumbnail?id=1b7OD6bCUiO1o0haO5vy6D_nmrnvkSk8T", system: "atari2600" },
+    { title: "DEFENDER 2", img: "https://drive.google.com/thumbnail?id=1T6qq991survfLw_I7MhfAS103xdY4jFq", system: "atari2600" },
+    { title: "DEMOLITION HERBY", img: "https://drive.google.com/thumbnail?id=1BhCkl3muOKpGZPoSQ_hxjz2Wof50rhZ_", system: "atari2600" },
+    { title: "DEMON ATAK", img: "https://drive.google.com/thumbnail?id=1EedAHYcY4_PrDi-CLNgrFKfc4UjVQ1ij", system: "atari2600" },
+    { title: "DICE PUZZLE", img: "https://drive.google.com/thumbnail?id=1FbHj-TVE1dHUzDoWsV8VIYsD4ZXSgs1G", system: "atari2600" },
+    { title: "DIG DUG", img: "https://drive.google.com/thumbnail?id=1TAmK0VqvxS0CLD1RVu7nuWLDBgAlKGb9", system: "atari2600" },
+    { title: "DODGE EM", img: "https://drive.google.com/thumbnail?id=1WeMzEUHcCPLw119G3ZuKtehZDJhsiblr", system: "atari2600" },
+    { title: "DONKEY KONG", img: "https://drive.google.com/thumbnail?id=1AZh40KbZjcK2PsEKGS_u_4se01NvTjl2", system: "atari2600" },
+    { title: "DRAGONFIRE", img: "https://drive.google.com/thumbnail?id=1Ab-usEHrvVSKBxKY3VlljnBB9lPqrE1H", system: "atari2600" },
+    { title: "ELK ATTAK", img: "https://drive.google.com/thumbnail?id=1YKj2ve4EloEARhYysdBCjtxidvY0GkWw", system: "atari2600" },
+    { title: "ENDURO", img: "https://drive.google.com/thumbnail?id=1uWLgfHAKeuU85vutFN19aCFLoddoZNIh", system: "atari2600" },
+    { title: "FALLDOWN", img: "https://drive.google.com/thumbnail?id=14_yT2lpp4MhtvPR-KOuahQILtJzfNGDa", system: "atari2600" },
+    { title: "FLASH GORDON", img: "https://drive.google.com/thumbnail?id=1BAaEJBICsyJoFMMZ0BjP9pG8gA-0cB5r", system: "atari2600" },
+    { title: "FROGGER", img: "https://drive.google.com/thumbnail?id=1gmiZXl2t5lwX5COPvjuh22usNtW9JfaA", system: "atari2600" },
+    { title: "FROGGER 2", img: "https://drive.google.com/thumbnail?id=1_a7zEbWmmS9BlSu2zkc7sXl19vQ2GctS", system: "atari2600" },
+    { title: "NOMBRE", img: "https://drive.google.com/thumbnail?id= IMAGEN", system: "atari2600" },
+    { title: "NOMBRE", img: "https://drive.google.com/thumbnail?id= IMAGEN", system: "atari2600" },
+    
+    
 ]);
+
+
+
+
+   
+            
+            
+         
+            
