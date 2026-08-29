33d8c134f936a99c1add007885a1cd251f94a078
retrostation-sys <retrostationperoiz74@gmail.com>
2026-08-29 07:12:01 -0600

    Add files via upload

---
+++ juegos-mame.js
@@ -0,0 +1,26 @@
+// https://ia601900.us.archive.org/view_archive.php?archive=/2/items/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip
+
+x= "https://drive.google.com/thumbnail?id=1";
+agregarJuegos([
+	{ system:"mame", img:x+"", title: "1942",  src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/1942.zip" },
+	{ system:"mame", img:x+"", title:"ALTERED BEAST", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/altbeast.zip" },
+	{ system:"mame", img:x+"", title:"POW", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/pow.zip" },
+	{ system:"mame", img:x+"", title:"TEKKEN 2", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/tekken2.zip" },
+	{ system:"mame", img:x+"", title:"TEKKEN 3", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/tekken3.zip" },
+	{ system:"mame", img:x+"", title:"TIME PILOT", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/timeplt.zip" },
+	{ system:"mame", img:x+"", title:"R-TYPE 2", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/rtype2.zip" },
+		{ system:"mame", img:x+"", title:"R-TYPE LEO", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/rtypeleo.zip" },
+	
+	
+    
+    		
+    
+
+    
+    
+    
+    
+    
+
+
+]);
