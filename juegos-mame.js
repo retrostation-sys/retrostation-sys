f4b6cede28d2ba0f3217a5831efc9164c55be735
retrostation-sys <retrostationperoiz74@gmail.com>
2026-08-30 06:59:27 -0600

    Add files via upload

--- juegos-mame.js
+++ juegos-mame.js
@@ -2,6 +2,7 @@
 
 x= "https://drive.google.com/thumbnail?id=1";
 agregarJuegos([
+	 { system:"mame", img:x+"", title:"3 WONDERS",  src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/3wonderu.zip" },
 	{ system:"mame", img:x+"", title: "1942",  src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/1942.zip" },
 	{ system:"mame", img:x+"", title:"ALTERED BEAST", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/altbeast.zip" },
 	{ system:"mame", img:x+"", title:"POW", src:"https://archive.org/download/ultimate-rom-collection/Arcade%20-%20Mame%202003%20Plus.zip/pow.zip" },
---
+++ juegos-psx.js
@@ -0,0 +1,9 @@
+// https://ia801900.us.archive.org/view_archive.php?archive=/2/items/ultimate-rom-collection/Nintendo%20-%20NES.zip
+x= "https://drive.google.com/thumbnail?id=1";
+agregarJuegos([   
+    { system:"psx", img:x+"461XafOG4qdZF1Gs1vEqaMLiQ9U3xie4", title: "ACE COMBAT",  src:"https://archive.org/download/ultimate-rom-collection/Sony%20-%20PS1/Ace%20Combat%202%20%28USA%29.chd" },
+    
+    
+
+
+]);
