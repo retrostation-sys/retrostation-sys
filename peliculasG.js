9ccc13fc6ff60388edc3061d4a5c9c6e8101fae3
retrostation-sys <retrostationperoiz74@gmail.com>
2026-09-02 16:38:39 -0600

    Add files via upload

--- peliculasG.js
+++ peliculasG.js
@@ -1,20 +1,21 @@
 const data_peliculasG= [
-	{ id: 'PelículasG',
-		data: [
-			{ id: '1SB7FuVM6fQE9ssU5Q3FtEozKH7z5UHsd', youtubeId: 'mhQcpvKHis4', buttonId: '1ZYW9FIM-81WCslsVZuoeiA', title: 'G20', subtitle: 'G20 (2025)', buscar: 'PEROIZG' }, {
-								id: '1_Km6ZX2mZ1FcRzmewgxPbLlqxsIxq1iq', youtubeId: 'Gq-b_iNrQaQ', buttonId: '1STRwHEaWgLqpQkjUP2ghIg', title: 'GALAXY EXPRESS 999', subtitle: 'El Expreso Galáctico (1978) [Jap T+Esp]', subtitle2: '1 Temporada 112 Episodios', buscar: 'S3R01' }, {
-								id: '1lBRMFOYXdqdE4QFmU1fl_nkfS9PLRoCh', youtubeId: '4AdIgtVP8aQ', buttonId: '16gs65Q39e95VkxkHnFpYkw', title: 'GAMERA REBIRTH', subtitle: 'Gamera Renacimiento (2023)', subtitle2: '1 Temporada 6 Episodios', buscar: 'S3R01' }, {
-								id: '1V2dI0ebOcNmbCKfIEj9XkSaNkA3nTNyf', youtubeId: 'ITlM041DW1U', buttonId: '1nxLIHgLajkHc2dfC5HuAIw', title: 'GEORGE OF THE JUNGLE', subtitle: 'George De La Selva (1997)', buscar: 'P3L02' }, {
-								id: '1S9uyVZvI98IaVxRVK4Ladot0Es9NOPjo', youtubeId: 'Z_KAzu2rq30', buttonId: '18sf1ScqXJbzgS5lHoTk54g', title: 'GHOST GAME', subtitle: 'El Mal Vive Aquí (2024)', buscar: 'P3L01G' }, {
-								id: '1CRl8uNhQABNe6C7Orvd5sT14SMz099tX', youtubeId: 'z7GbDaCYChw', buttonId: '1RQSpFJ2vvgF7pfZoqq4Fbw', title: 'GIANNIS THE MARVELOUS JOURNEY', subtitle: 'Giannis El Viaje Maravilloso (2024)', buscar: 'P3L02' }, {
-								id: '1CTRIp_su6WYPobuANIg8NXG7Wa-68Uus', youtubeId: 'TEq5wtFr9e8', buttonId: '1VaAXi1UY_4UAhPhZAENwyA', title: 'GILLIGAN\'S ISLAND', subtitle: 'La Isla De Gilligan (1964)', subtitle2: '3 Temporadas 98 Episodios', buscar: 'S3R01' }, {
-								id: '1CdHqYLfI5v3V4E4Uo4BsJUxwLf687rH_', youtubeId: '1OfIUUnGOBQ', buttonId: '1yBSXEOf5tVxgneif_5wpdw', title: 'GINTAMA', subtitle: 'Gintama (2017)', subtitle2: 'Español, Japonés sub: Español, Inglés', buscar: 'P3L01' }, {
-								id: '1CgHyy7iFFhM8__odXEi9vZ-Pj-Vf5xu6', youtubeId: 'o4O4PYOqDMQ', buttonId: '1KOTQSD_0_i6fyVNPp1ciTA', title: 'GIRL IN THE BUNKER', subtitle: 'La Chica Del Búnker (2018)', buscar: 'P3L01' }, {
-								id: '1Chhaski8GD-rMsDFYle6x1m2d4FxcUU7', youtubeId: '9X0GMOcZnYw', buttonId: '1_5PhW4raht7rnoSrFbfetg', title: 'GIRLS WITH BALLS', subtitle: 'Girls With Balls (2019)', buscar: 'P3L01' }, {
-								id: '1CjKcokctko2aEySgSiCpiFO_-3B_RlX_', youtubeId: 'qwMbpO20_YY', buttonId: '1VuabPr0cP8zXeSid89DTcQ', title: 'GLADIATOR 2', subtitle: 'Gladiador 2 (2024)', buscar: 'P3L01' }, {
-								id: '1Ckc927HAGCtFp-2C2SQMeWSTM_cit6wh', youtubeId: 'cKQaJeHOXuA', buttonId: '1yc5NSTDDN0Msq-ivtO4SuA', title: 'GLASS', subtitle: 'Glass (2019)', buscar: 'P3L01G' }, {
+     { id: 'PelículasG', data: [
+          { id: '1SB7FuVM6fQE9ssU5Q3FtEozKH7z5UHsd', youtubeId: 'mhQcpvKHis4', buttonId: '1ZYW9FIM-81WCslsVZuoeiA', title: 'G20', subtitle: 'G20 (2025)', buscar: 'peroiz74g' }, 
+          { id: '1_Km6ZX2mZ1FcRzmewgxPbLlqxsIxq1iq', youtubeId: 'Gq-b_iNrQaQ', buttonId: '1STRwHEaWgLqpQkjUP2ghIg', title: 'GALAXY EXPRESS 999', subtitle: 'El Expreso Galáctico (1978) [Jap T+Esp]', subtitle2: '1 Temporada 112 Episodios', buscar: 'S1' }, 
+          { id: '1lBRMFOYXdqdE4QFmU1fl_nkfS9PLRoCh', youtubeId: '4AdIgtVP8aQ', buttonId: '16gs65Q39e95VkxkHnFpYkw', title: 'GAMERA REBIRTH', subtitle: 'Gamera Renacimiento (2023)', subtitle2: '1 Temporada 6 Episodios', buscar: 'S1' }, 
+          { id: '1V2dI0ebOcNmbCKfIEj9XkSaNkA3nTNyf', youtubeId: 'ITlM041DW1U', buttonId: '1nxLIHgLajkHc2dfC5HuAIw', title: 'GEORGE OF THE JUNGLE', subtitle: 'George De La Selva (1997)', buscar: 'P2' }, 
+          { id: '1S9uyVZvI98IaVxRVK4Ladot0Es9NOPjo', youtubeId: 'Z_KAzu2rq30', buttonId: '18sf1ScqXJbzgS5lHoTk54g', title: 'GHOST GAME', subtitle: 'El Mal Vive Aquí (2024)', buscar: 'P1G' }, 
+          { id: '1CRl8uNhQABNe6C7Orvd5sT14SMz099tX', youtubeId: 'z7GbDaCYChw', buttonId: '1RQSpFJ2vvgF7pfZoqq4Fbw', title: 'GIANNIS THE MARVELOUS JOURNEY', subtitle: 'Giannis El Viaje Maravilloso (2024)', buscar: 'P2' }, 
+          { id: '1CTRIp_su6WYPobuANIg8NXG7Wa-68Uus', youtubeId: 'TEq5wtFr9e8', buttonId: '1VaAXi1UY_4UAhPhZAENwyA', title: 'GILLIGAN\'S ISLAND', subtitle: 'La Isla De Gilligan (1964)', subtitle2: '3 Temporadas 98 Episodios', buscar: 'S1' }, 
+          { id: '1CdHqYLfI5v3V4E4Uo4BsJUxwLf687rH_', youtubeId: '1OfIUUnGOBQ', buttonId: '1yBSXEOf5tVxgneif_5wpdw', title: 'GINTAMA', subtitle: 'Gintama (2017)', subtitle2: 'Español, Japonés sub: Español, Inglés', buscar: 'P1' },
+          { id: '1CgHyy7iFFhM8__odXEi9vZ-Pj-Vf5xu6', youtubeId: 'o4O4PYOqDMQ', buttonId: '1KOTQSD_0_i6fyVNPp1ciTA', title: 'GIRL IN THE BUNKER', subtitle: 'La Chica Del Búnker (2018)', buscar: 'P1' },
+          { id: '1Chhaski8GD-rMsDFYle6x1m2d4FxcUU7', youtubeId: '9X0GMOcZnYw', buttonId: '1_5PhW4raht7rnoSrFbfetg', title: 'GIRLS WITH BALLS', subtitle: 'Girls With Balls (2019)', buscar: 'P1' },
+          { id: '19O87pwqI5YSNsPhr9fx8uWcJgDZ-_eCH_', youtubeId: 'TcYIqlBihW4', buttonId: '1qDNmNeygzBBqF-R4t66VsA', title: 'GLADIATOR', subtitle: 'Gladiador 2 (2000)', buscar: 'peroiz74g' },
+          { id: '1CjKcokctko2aEySgSiCpiFO_-3B_RlX_', youtubeId: 'qwMbpO20_YY', buttonId: '1VuabPr0cP8zXeSid89DTcQ', title: 'GLADIATOR 2', subtitle: 'Gladiador 2 (2024)', buscar: 'P1' },
+          { id: '1Ckc927HAGCtFp-2C2SQMeWSTM_cit6wh', youtubeId: 'cKQaJeHOXuA', buttonId: '1yc5NSTDDN0Msq-ivtO4SuA', title: 'GLASS', subtitle: 'Glass (2019)', buscar: 'P3L01G' }, {
 								id: '1_sqqahdsKB8yBZSMYQ3Xyyho3rTs2WXp', youtubeId: 'aaFrYWbevHE', buttonId: '1UL4J2GzFxFWvBCas2KJlxw', title: 'GODZILLA', subtitle: 'Godzilla (1998)', buscar: 'P3L01' }, {
 								id: '1Co4LawiUA9fVI0efBa_O4OWKZgsjjw_x', youtubeId: 'ZkIe8xlfolU', buttonId: '1Tx4wTlxyR9Celp-b9zHAzQ', title: 'GODZILLA THE PLANET EATER', subtitle: 'Godzilla El Devorador De Planetas (2018)', buscar: 'P3L01' }, {
+								id: '1xUzHNtG1yVWYGUdaT1OFp6fDgfBto55d', youtubeId: 'Kqg8zLgESyQ', buttonId: '1lUkzWFXPCHZ7ABySxoGkdg', title: 'GODZILLA VS KONG', subtitle: 'Godzilla Vs Kong (2021)', buscar: 'King Kong peroiz74g' }, {
 								id: '1CkMXvtrZrhY0GlKGvBX7ozJqy0CFr3Dx', youtubeId: 'TEACtsg1tQ4', buttonId: '1IyRSabenDkvvpa_ZD4kstQ', title: 'GONE GIRL', subtitle: 'Perdida (2014)', buscar: 'P3L01G' }, {
 								id: '1zkNEhYwTjhTTmLJZGvlFEEbCSk6ZWUK_', youtubeId: '9a4M-Em-aqM', buttonId: '1O2C_TyKnccgDnVgbOeiFoQ', title: 'GOLDBEAK', subtitle: 'Entre Picos y Plumas (2021)', buscar: 'PEROIZG' }, {
 								id: '1Mv3BNXbCsp6_aB6lj3JCmsPerVSAm6DQ', youtubeId: 'H0qahz8NI0Q', buttonId: '15R9DGttp4BlsmrhL5UnP-A', title: 'GOOD LUCK CHARLIE!', subtitle: 'Buena Suerte Charlie! (2010)', subtitle2: '4 Temporadas 97 Episodios', buscar: 'S3R01' }, {
