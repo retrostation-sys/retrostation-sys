b4166406597930d19d4b14cbd3262acb45fa4070
retrostation-sys <retrostationperoiz74@gmail.com>
2026-09-08 14:05:05 -0600

    Add files via upload

--- juegos-psx.js
+++ juegos-psx.js
@@ -4,7 +4,7 @@
 x="https://drive.google.com/thumbnail?id=1";
 y="https://archive.org/download/ultimate-rom-collection/Sony%20-%20PS1%20%28Update%201%29.zip/";
 agregarJuegos([   
-    { system:"psx", img:x+"e6csb4UTH-7PGiMoFt6uxXGG2ni5UHqj", title:"X-COM UFO DEFENSE",  src:"X-COM%20-%20UFO%20Defense%20%28USA%29.m3u" },
+    { system:"psx", img:x+"e6csb4UTH-7PGiMoFt6uxXGG2ni5UHqj", title:"X-COM UFO DEFENSE",  src:"X-COM%20-%20UFO%20Defense%20%28USA%29.chd" },
     
     
 
