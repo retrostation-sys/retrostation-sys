d4cbea1603cbcd96c7b48e6f68750bf840a5d6df
retrostation-sys <retrostationperoiz74@gmail.com>
2026-09-08 14:02:07 -0600

    Add files via upload

--- juegos-psx.js
+++ juegos-psx.js
@@ -1,7 +1,10 @@
 // https://ia801900.us.archive.org/view_archive.php?archive=/2/items/ultimate-rom-collection/Nintendo%20-%20NES.zip
-x= "https://drive.google.com/thumbnail?id=1";
+
+
+x="https://drive.google.com/thumbnail?id=1";
+y="https://archive.org/download/ultimate-rom-collection/Sony%20-%20PS1%20%28Update%201%29.zip/";
 agregarJuegos([   
-    { system:"psx", img:x+"", title: "ACE COMBAT",  src:"https://archive.org/download/ultimate-rom-collection/Sony%20-%20PS1%20%28Update%201%29.zip/Fighting%20Force%20%28USA%29%20%28Rev%202%29.chd" },
+    { system:"psx", img:x+"e6csb4UTH-7PGiMoFt6uxXGG2ni5UHqj", title:"X-COM UFO DEFENSE",  src:"X-COM%20-%20UFO%20Defense%20%28USA%29.m3u" },
     
     
 
