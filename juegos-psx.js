229ae86ec77515e9fb8014ed48d833b9b51871ea
retrostation-sys <retrostationperoiz74@gmail.com>
2026-09-09 16:49:05 -0600

    Add files via upload

--- juegos-psx.js
+++ juegos-psx.js
@@ -1,12 +1,47 @@
-// https://ia801900.us.archive.org/view_archive.php?archive=/2/items/ultimate-rom-collection/Nintendo%20-%20NES.zip
-
+// https://ia801900.us.archive.org/view_archive.php?archive=/2/items/ultimate-rom-collection/Sony%20-%20PS1%20%28Update%201%29.zip
+//https://archive.org/details/psx-chd-roms-r
 
 x="https://drive.google.com/thumbnail?id=1";
 y="https://archive.org/download/ultimate-rom-collection/Sony%20-%20PS1%20%28Update%201%29.zip/";
-agregarJuegos([   
-    { system:"psx", img:x+"e6csb4UTH-7PGiMoFt6uxXGG2ni5UHqj", title:"X-COM UFO DEFENSE",  src:"X-COM%20-%20UFO%20Defense%20%28USA%29.chd" },
-    
-    
+
+
+agregarJuegos([
+    { system:"psx", img:x+"F3cJMILeajZuvt72NnakYsOWye5pKPpu", title:"¡S INTERNAL SECTION", src:y+"iS%20-%20Internal%20Section%20%28Japan%29%20%5BT-En%20by%20GameHacking.org%20v1.02%5D.chd" },
+    { system:"psx", img:x+"-0Dligj5SJLVl2ge8K8pSQ3EDkITvsnZ", title:"FIGHTING FORCE",  src:y+"Fighting%20Force%20%28USA%29%20%28Rev%202%29.chd" },
+    { system:"psx", img:x+"w-ghwDp9B1lc0djeaUJe8oFxXUd_8_0y", title:"FRONT MISSION 2",  src:y+"Front%20Mission%202%20%28Japan%29%20%5BT-En%20by%20Front%20Mission%205%20Team%20Patch%20F%5D.chd" },
+    { system:"psx", img:x+"E-Ksv0aee3Db8vphip6WwabHmG_nwgdA", title:"GUNDAM BATTLE ASSAULT",  src:y+"Gundam%20Battle%20Assault%20%28USA%29.chd" },
+    { system:"psx", img:x+"NpWi2h8xsqppw12BQ8kGiGri4cQW5pf0", title:"GUNDAM BATTLE ASSAULT 2",  src:y+"Gundam%20Battle%20Assault%202%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"HARMFUL PARK",  src:y+"Harmful%20Park%20%28Japan%29%20%5BT-En%20by%20Hilltop%20v1.1%5D.chd" },
+    { system:"psx", img:x+"", title:"IN COLD BLOOD (D1)",  src:y+"In%20Cold%20Blood%20%28USA%29%20%28Disc%201%29.chd" },
+    { system:"psx", img:x+"", title:"IN COLD BLOOD (D2)",  src:y+"In%20Cold%20Blood%20%28USA%29%20%28Disc%202%29.chd" },
+    { system:"psx", img:x+"", title:"JADE COCOON",  src:y+"Jade%20Cocoon%20-%20Story%20of%20the%20Tamamayu%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"MAGIC CARPET",  src:y+"Magic%20Carpet%20%28USA%29%20%28En%2CFr%2CDe%2CEs%2CSv%29.chd" },
+    { system:"psx", img:x+"", title:"R-C STUNT COPTER",  src:y+"R-C%20Stunt%20Copter%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"SAGA FRONTIER",  src:y+"SaGa%20Frontier%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"SAGA FRONTIER 2",  src:y+"SaGa%20Frontier%202%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"SKULLMONKEYS",  src:y+"Skullmonkeys%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"SPEED PUNKS",  src:y+"Speed%20Punks%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"TAIL CONCERTO",  src:y+"Tail%20Concerto%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"TEAM BUDDIES",  src:y+"Team%20Buddies%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"TENCHU STEALTH ASSASSINS",  src:y+"Tenchu%20-%20Stealth%20Assassins%20%28USA%29%20%28Rev%201%29.chd" },
+    { system:"psx", img:x+"", title:"TENCHU 2 BIRTH OF THE STEALTH ASSASSINS",  src:y+"Tenchu%202%20-%20Birth%20of%20the%20Stealth%20Assassins%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"THUNDER FORCE V PERFECT SYSTEM",  src:y+"Thunder%20Force%20V%20-%20Perfect%20System%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"TOBAL NO. 1",  src:y+"Tobal%20No.%201%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"TRAP GUNNER",  src:y+"Trap%20Gunner%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"TWISTED METAL",  src:y+"Twisted%20Metal%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"WAR GAMES DEFCON 1",  src:y+"WarGames%20-%20Defcon%201%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"X-COM UFO DEFENSE",  src:y+"X-COM%20-%20UFO%20Defense%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"ZANAC X ZANAC",  src:y+"Zanac%20x%20Zanac%20%28Japan%29.chd" },
+    { system:"psx", img:x+"", title:"CENTURON",  src:"https://archive.org/download/psx-chd-roms-r/Rushdown%20%28USA%29.chd" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
+    { system:"psx", img:x+"", title:"",  src:y+"" },
 
 
 ]);
